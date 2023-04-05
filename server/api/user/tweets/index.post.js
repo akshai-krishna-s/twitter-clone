d5da3ac6b2d5fd63279180d0f7603d946bc4bf16
Akshai-Krishna-S <akshai404akz@gmail.com>
2023-04-06 00:18:37 +0530

    started working on tweet

---
+++ server/api/user/tweets/index.post.js
@@ -0,0 +1,23 @@
+import formidable from 'formidable'
+
+export default defineEventHandler(async (event) => {
+  const form = formidable({})
+
+  const response = await new Promise((resolve, reject) => {
+    form.parse(event.node.req, (err, fields, files) => {
+      if (err) {
+        reject(err)
+      }
+
+      resolve({ fields, files })
+    })
+  })
+
+  const { fields, files } = response
+
+  const userId = event.context?.auth?.user.id
+
+  return {
+    userId,
+  }
+})
--- server/transformers/user.js
+++ server/transformers/user.js
@@ -1,3 +1,11 @@
+// userTransformer takes in a user object and returns an object containing only the id, username, email, name and profileImage properties of the user object.
+/**
+ * This function transforms the user object to return only the required
+ * properties to the client.
+ *
+ * @param {Object} user - The user object.
+ * @returns {Object} - The transformed user object.
+ */
 export const userTransformer = (user) => {
   return {
     id: user.id,
